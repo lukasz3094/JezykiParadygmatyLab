64c494e1ff4548a658c85a273c20206d838b1ba8
Luke_127 <74059306+lukasz3094@users.noreply.github.com>
2025-01-18 16:01:33 +0100

    Add files via upload
    lab7

---
+++ Lab6/Lab6/Program.fs
@@ -0,0 +1,29 @@
+﻿//[<AbstractClass>]
+//type Pojazd(typPojazdu: string) =
+//    member val TypPojazdu = typPojazdu with get, set    
+//    abstract member Info: unit -> string
+
+//type Samochod(typPojazdu: string, marka: string, model: string, rok: int) =
+//    inherit Pojazd(typPojazdu)
+
+//    member val Marka = marka with get, set
+//    member val Model = model with get, set
+//    member val Rok = rok with get, set
+
+//    override this.Info() = $"{this.TypPojazdu}: {this.Marka} {this.Model} {this.Rok}"
+
+//    member this.Opis() = $"{this.TypPojazdu}: {this.Marka} {this.Model} {this.Rok}"
+
+//let samochod1 = Samochod("Samochód","Audi", "A4", 2010)
+
+//printfn "%s" (samochod1.Opis())
+
+//type IWydruk =
+//    abstract member Drukuj: unit -> unit
+
+
+//type Dokument(tresc: string) =
+//    interface IWydruk with
+//        member this.Drukuj() = printfn "Drukowany jest tekst z klasy Dokument %s" tresc
+
+Zad1.main()
---
+++ Lab6/Lab6/Zad1.fs
@@ -0,0 +1,66 @@
+﻿module Zad1
+
+type Book(tytul: string, autor: string, liczbaStron: int) =
+    member val Tytul = tytul with get, set
+    member val Autor = autor with get, set
+    member val LiczbaStron = liczbaStron with get, set
+
+    member this.GetInfo() = $"{this.Tytul} {this.Autor} {this.LiczbaStron}"
+
+type User(imie: string, nazwisko: string) =
+    let mutable borrowedBooks = []
+    member this.BorrowedBooks = borrowedBooks
+
+    member val Imie = imie with get, set
+    member val Nazwisko = nazwisko with get, set
+
+    member this.BorrowBook(book: Book) = 
+        borrowedBooks <- book :: borrowedBooks
+        printfn "Użytkownik %s %s wypożyczył książkę %s" this.Imie this.Nazwisko (book.GetInfo())
+
+    member this.ReturnBook(book: Book) = 
+        borrowedBooks <- List.filter (fun b -> b <> book) borrowedBooks
+        printfn "Użytkownik %s %s zwrócił książkę %s" this.Imie this.Nazwisko (book.GetInfo())
+
+type Library(users: List<User>) =
+    let mutable books = []
+    let mutable users = users
+
+    member val Books = books with get, set
+    member val Users = users with get, set
+
+    member this.AddBook(book: Book) = this.Books <- book :: this.Books
+    member this.RemoveBook(book: Book) =  this.Books <- List.filter (fun b -> b <> book)  this.Books
+   
+    member this.GetBorrowedBooks() = this.Users |> List.collect (fun (u: User) -> u.BorrowedBooks)
+    member this.ListBooks() = this.Books |> List.iter (fun b -> printfn "%s" (b.GetInfo()))
+    member this.ListBorrowedBooks() = this.GetBorrowedBooks() |> List.iter (fun b -> printfn "%s" (b.GetInfo()))
+    member this.ListAvailableBooks() = this.Books |> List.filter (fun b -> 
+        not (this.GetBorrowedBooks() |> List.exists (fun bb -> bb = b))) |> List.iter (fun b -> printfn "%s" (b.GetInfo()))
+
+let main() =
+    let user = User("Jan", "Kowalski")
+    let library = Library([user])
+
+    let book1 = Book("Wiedźmin", "Andrzej Sapkowski", 300)
+    let book2 = Book("Harry Potter", "J.K. Rowling", 400)
+    let book3 = Book("Hobbit", "J.R.R. Tolkien", 350)
+
+    library.AddBook(book1)
+    library.AddBook(book2)
+    library.AddBook(book3)
+
+    printfn "Lista książek:\n"
+    library.ListBooks()
+
+    printfn ""
+
+    user.BorrowBook(book1)
+    user.BorrowBook(book2)
+
+    user.ReturnBook(book1)
+
+    printfn "\n---Lista wypożyczonych książek---\n"
+    library.ListBorrowedBooks()
+    printfn "\n---Lista dostępnych książek---\n"
+    library.ListAvailableBooks()
